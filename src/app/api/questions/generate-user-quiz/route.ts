cf33809a83214f8ddba206fd71881a599b58747b
MoschellaV <58868225+MoschellaV@users.noreply.github.com>
2023-05-27 16:27:33 -0400

    ai generated questions (mcq + input) styling changes loading spinner

--- src/app/api/questions/generate-user-quiz/route.ts
+++ src/app/api/questions/generate-user-quiz/route.ts
@@ -6,13 +6,25 @@ export const POST = async (request: Request) => {
 
   const body = await request.json();
 
+  const prompt = `I want to build a habit of ${body.params.prompt}. Create a valid JSON array array of 10 objects each object should contain two attributes, a question and options. The question should gather more information about me and also ask some questions regarding specific constraints that may limit my ability to build my habit plan (a question can either require input or be multiple choice but never be both, multiple choice questions cannot be have input, and questions that require input cannot have multiple options). The option attribute should be an array of options  only if the question is multiple choice. If the question requires an input, the option attribute should have a value of  'input required'. Now generate some multiple choice questions and some questions that require an input.  A question that requires an input and is not multiple choice MUST NOT have any other options. If a question requires an input there must only be one option and it should be 'input required'. Understand that you need to generate at least two questions that requires an input, please remember to generate at least two questions that require an input.'.`;
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: body.params.prompt,
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
     });
 
-    return NextResponse.json({ data: completion.data.choices[0] }, { status: 200 });
+    const content = completion.data.choices[0].message?.content || null;
+    if (content) {
+      const generatedQuestions = JSON.parse(content);
+      return NextResponse.json({ data: generatedQuestions }, { status: 200 });
+    } else {
+      return NextResponse.json("Could not generate questions");
+    }
   } catch (error) {
     console.error(error);
     return NextResponse.json(error);
--- src/components/UserQuestions/UserFlow.tsx
+++ src/components/UserQuestions/UserFlow.tsx
@@ -1,14 +1,60 @@
 "use client";
+import React, { useState } from "react";
 import MainSearch from "../../components/UserQuestions/MainSearch";
 import Medium from "../../components/UserQuestions/Medium";
 import Question from "../../components/UserQuestions/Question";
+import axios from "axios";
+import type { OpenAIApi } from "openai";
+import { scrollDown } from "~/src/utils/helper";
+
+type GeneratedUserQuiz = Awaited<ReturnType<OpenAIApi["createCompletion"]>>;
 
 const UserFlow = () => {
+  const [promptValue, setPromptValue] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState(null);
+
+  const submitHabit = async () => {
+    setLoading(true);
+
+    try {
+      const prompt = `I want to build a habit of ${promptValue}`;
+      const response = await axios.post<GeneratedUserQuiz>("/api/questions/generate-user-quiz/", {
+        params: {
+          prompt: prompt,
+        },
+      });
+
+      // console.log(response.data.data);
+
+      setQuestions(response.data.data);
+      setLoading(false);
+      scrollDown();
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    }
+  };
+
+  const renderQuestions = () => {
+    if (questions) {
+      return questions.map((question, index) => (
+        <Question key={index} question={question.question} options={question.options} />
+      ));
+    }
+  };
+
   return (
     <>
-      <MainSearch />
+      <MainSearch
+        loading={loading}
+        promptValue={promptValue}
+        setPromptValue={setPromptValue}
+        submitHabit={submitHabit}
+      />
       <Medium />
-      <Question />
+
+      {renderQuestions()}
     </>
   );
 };
