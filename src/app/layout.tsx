93c44f62a69ba9133fd314c4387bcaab8c2fbca8
MoschellaV <58868225+MoschellaV@users.noreply.github.com>
2023-05-26 21:55:27 -0400

    bugfix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,25 +6,25 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "HabitMaker",
-  description: "Make habits with the power of AI",
+    title: "HabitMaker",
+    description: "Make habits with the power of AI",
 };
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
-  return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>
-          <div className="min-h-screen">
-            <div className="flex justify-end p-2">
-              <UserButton afterSignOutUrl="/" />
-            </div>
-            <div className="flex-1 flex flex-col">{children}</div>
-          </div>
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <body className={inter.className}>
+                    <div className="min-h-screen">
+                        <div className="flex justify-end p-2">
+                            <UserButton afterSignOutUrl="/" />
+                        </div>
+                        <div className="flex-1 flex flex-col">{children}</div>
+                    </div>
+                </body>
+            </html>
+        </ClerkProvider>
+    );
 };
 
 export default Layout;
