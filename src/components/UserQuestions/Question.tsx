2b593cc5fadcffbf6e7def750d14b32ee6d4f56e
Conor Roberts <Conor@ConorRoberts.com>
2023-05-27 18:54:07 -0400

    type updates

--- src/components/UserQuestions/Question.tsx
+++ src/components/UserQuestions/Question.tsx
@@ -20,66 +20,74 @@ const CustomOptionButton = styled(Button)(({ theme }) => ({
   justifyContent: "flex-start",
 }));
 
+const OptionsList: FC<{ setInputValue: (str: string) => void; options: string[] | string; inputValue: string }> = ({
+  options,
+  setInputValue,
+  inputValue,
+}) => {
+  const theme = useTheme();
+  if (options[0] === "input required" || options === "input required") {
+    return (
+      <Box>
+        <Input
+          sx={{
+            "& .MuiInput-underline:before": {
+              borderBottomColor: theme.palette.primary.main,
+            },
+            "&:focus-within .MuiInput-underline:before": {
+              borderBottomColor: theme.palette.primary.main,
+            },
+            "& .MuiInput-underline:after": {
+              borderBottomColor: theme.palette.primary.main,
+            },
+            "& .MuiInputBase-input": {
+              color: "#000",
+              paddingBottom: 0,
+              fontSize: { xs: 19, sm: 23, md: 25, lg: 25 },
+              width: { xs: "100%", lg: 520 },
+            },
+          }}
+          placeholder="Your resposne"
+          autoComplete="off"
+          value={inputValue}
+          onChange={(event) => {
+            setInputValue(event.target.value);
+          }}
+          onKeyDown={(event) => {
+            if (event.key === "Enter" && inputValue) {
+              scrollDown();
+              // would need to store the input some place
+              setInputValue(""); // and clear state
+            }
+          }}
+        />
+        <IconButton aria-label="enter" size="large" onClick={scrollDown}>
+          <SubdirectoryArrowLeftIcon sx={{ fontSize: 29 }} />
+        </IconButton>
+      </Box>
+    );
+  } else if (Array.isArray(options)) {
+    return (
+      <>
+        {options.map((option, index) => (
+          <CustomOptionButton key={index} variant="text" size="large" onClick={scrollDown}>
+            <Typography variant="h5" component="h5" sx={{ fontSize: { xs: 19, sm: 23, md: 25, lg: 25 } }}>
+              {option}
+            </Typography>
+          </CustomOptionButton>
+        ))}
+      </>
+    );
+  }
+
+  return null;
+};
+
 const Question: FC<{ question: string; options: string[] | string }> = ({ question, options }) => {
   const [inputValue, setInputValue] = useState("");
   const theme = useTheme();
 
   console.log(options);
-
-  const renderOptions = (options: typeof options) => {
-    if (options[0] === "input required" || options === "input required") {
-      return (
-        <Box>
-          <Input
-            sx={{
-              "& .MuiInput-underline:before": {
-                borderBottomColor: theme.palette.primary.main,
-              },
-              "&:focus-within .MuiInput-underline:before": {
-                borderBottomColor: theme.palette.primary.main,
-              },
-              "& .MuiInput-underline:after": {
-                borderBottomColor: theme.palette.primary.main,
-              },
-              "& .MuiInputBase-input": {
-                color: "#000",
-                paddingBottom: 0,
-                fontSize: { xs: 19, sm: 23, md: 25, lg: 25 },
-                width: { xs: "100%", lg: 520 },
-              },
-            }}
-            placeholder="Your resposne"
-            autoComplete="off"
-            value={inputValue}
-            onChange={(event) => {
-              setInputValue(event.target.value);
-            }}
-            onKeyDown={(event) => {
-              if (event.key === "Enter" && inputValue) {
-                scrollDown();
-                // would need to store the input some place
-                setInputValue(""); // and clear state
-              }
-            }}
-          />
-          <IconButton aria-label="enter" size="large" onClick={scrollDown}>
-            <SubdirectoryArrowLeftIcon sx={{ fontSize: 29 }} />
-          </IconButton>
-        </Box>
-      );
-    } else if (Array.isArray(options)) {
-      return options.map((option, index) => (
-        <CustomOptionButton key={index} variant="text" size="large" onClick={scrollDown}>
-          <Typography variant="h5" component="h5" sx={{ fontSize: { xs: 19, sm: 23, md: 25, lg: 25 } }}>
-            {option}
-          </Typography>
-        </CustomOptionButton>
-      ));
-    } else {
-      // handle other cases in case idk
-      return null;
-    }
-  };
 
   return (
     <>
@@ -107,7 +115,7 @@ const Question: FC<{ question: string; options: string[] | string }> = ({ questi
           </Typography>
 
           {/* QUESTIONS */}
-          {renderOptions(options)}
+          <OptionsList options={options} setInputValue={setInputValue} inputValue={inputValue} />
         </Box>
       </Box>
     </>
