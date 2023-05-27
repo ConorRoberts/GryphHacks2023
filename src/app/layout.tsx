5ea4cd1e6ebd3d572aeace1c9614d3f825c95574
Conor Roberts <Conor@ConorRoberts.com>
2023-05-26 21:00:20 -0400

    Add auth close #1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider, UserButton } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import { FC, PropsWithChildren } from "react";
 import "./globals.css";
@@ -11,9 +12,18 @@ export const metadata = {
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <div className="min-h-screen">
+            <div className="flex justify-end p-2">
+              <UserButton afterSignOutUrl="/" />
+            </div>
+            <div className="flex-1 flex flex-col">{children}</div>
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 };
 
