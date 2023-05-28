b76a6ef27c0a03bcba60a692b8f4bf5b0101dba3
Conor Roberts <Conor@ConorRoberts.com>
2023-05-28 09:13:55 -0400

    put it back to how it was

--- src/app/api/habits/HabitListElement.tsx
+++ src/app/api/habits/HabitListElement.tsx
@@ -6,8 +6,6 @@ import dayjs from "dayjs";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { FC, useState } from "react";
-import { Typography, Box } from "@mui/material";
-import { capitalizeWords } from "~/src/utils/helper";
 
 const HabitListElement: FC<{ data: Habit }> = (props) => {
   const router = useRouter();
@@ -24,24 +22,18 @@ const HabitListElement: FC<{ data: Habit }> = (props) => {
   };
 
   return (
-    <Link href={`/habits/${props.data.id}`} className="hover:bg-gray-50 transition duration-50 bg-white p-4 relative">
-      <Box sx={{ mt: 1.5, mb: 1.5 }}>
-        <Typography variant="h4" component="h3" sx={{ fontWeight: 700 }}>
-          {capitalizeWords(props.data.name)}
-        </Typography>
-        <Typography variant="h6" component="h5" sx={{ fontWeight: 500, fontSize: 16, opacity: 0.3 }}>
-          Complete by {dayjs(props.data.timeframe).format("MMM DD YYYY")}
-        </Typography>
-        <button
-          onClick={async (e) => {
-            e.preventDefault();
-            await deleteHabit();
-          }}
-          className="text-gray-400 hover:bg-gray-100 transition duration-75 rounded-full p-1 absolute top-2 right-2 z-10"
-        >
-          {isLoading ? <ArrowPathIcon className="w-5 h-5 animate-spin" /> : <TrashIcon className="w-5 h-5" />}
-        </button>
-      </Box>
+    <Link href={`/habits/${props.data.id}`} className="hover:bg-gray-50 transition duration-50 bg-white p-5 relative">
+      <h3 className="capitalize font-bold text-4xl">{props.data.name}</h3>
+      <p className="text-gray-500 text-sm">Complete by {dayjs(props.data.timeframe).format("MMM DD YYYY")}</p>
+      <button
+        onClick={async (e) => {
+          e.preventDefault();
+          await deleteHabit();
+        }}
+        className="text-gray-400 hover:bg-gray-100 transition duration-75 rounded-full p-1 absolute top-2 right-2 z-10"
+      >
+        {isLoading ? <ArrowPathIcon className="w-5 h-5 animate-spin" /> : <TrashIcon className="w-5 h-5" />}
+      </button>
     </Link>
   );
 };
